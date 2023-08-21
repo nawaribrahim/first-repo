6b6578718eae1d521f8ac3d4380cb347f875a5a5
nawaribrahim <nawaeali545@gmail.com>
2023-08-21 21:29:35 +0300

    Added CPP

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long
+vector<LL> v;
+int main()
+{
+    LL n;
+    cin>>n;
+    if (n == 1)
+        cout << 5;
+    else
+        cout << 25;
+    return 0;
+}
